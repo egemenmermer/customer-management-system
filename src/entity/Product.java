42d110bdfbdb4e71b097fa377783d29524e8fb8e
egemenmermer <egemenmermer@gmail.com>
2024-10-31 18:18:07 +0100

    added product entity

---
+++ src/entity/Product.java
@@ -0,0 +1,61 @@
+package entitiy;
+
+public class Product {
+    private String name;
+    private String code;
+    private int price;
+    private int stock;
+    private int id;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                ", id=" + id +
+                '}';
+    }
+}
